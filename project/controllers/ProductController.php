524ede76671e034842feb362b26e991190d9b1c4
&nbsp <you@example.com>
2019-03-20 14:19:38 +0300

    load yet

--- project/controllers/ProductController.php
+++ project/controllers/ProductController.php
@@ -8,17 +8,26 @@
 	{
 		protected $number = 3;
 		protected $page = 1;
-		protected $start;
+		protected $start = 0;
 
 		public function actionIndex(): void
 		{
 			if ($_GET['page'] !== null) {
 				$this->page = (int)$_GET['page'];
 			}
-			$this->start = $this->page * $this->number - $this->number;
-			$catalog = Products::getAll([$this->start, $this->number], 'category');
+		
+			
+			$this->number = $this->page * $this->number;
 
-			echo $this->render('catalog', ['catalog' => $catalog]);
+			$catalog = Products::getAll([(int)$this->start, (int)$this->number], 'category');
+			$this->page += 1;
+			$numProduct = count(Products::getAll()) - $this->number;
+	
+			echo $this->render('catalog', ['catalog' => $catalog, 
+			'counter' => [
+				'numProduct' => $numProduct, 
+				'pagesLeft' => $this->page]
+				]);
 		}
 
 		public function actionItem(): void
