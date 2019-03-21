ae7dcc4ef4955e78a54342263d540804cb8ddc76
&nbsp <you@example.com>
2019-03-21 18:03:51 +0300

    fix

--- project/controllers/ProductController.php
+++ project/controllers/ProductController.php
@@ -1,38 +1,43 @@
 <?php
 
-	namespace app\controllers;
+namespace app\controllers;
 
-	use app\model\Products;
+use app\model\Products;
 
-	class ProductController extends Controller
-	{
-		protected $number = 3;
-		protected $page = 1;
-		protected $start = 0;
+class ProductController extends Controller
+{
+    protected $number = 3;
+    protected $page = 1;
+    protected $loadItem = 3;
+    protected $start = 0;
 
-		public function actionIndex(): void
-		{
-			if ($_GET['page'] !== null) {
-				$this->page = (int)$_GET['page'];
-			}
-			
-			$this->number = $this->page * $this->number;
+    public function actionIndex(): void
+    {
+        if ($_GET['page'] !== null) {
+            $this->page = (int) $_GET['page'];
+        }
 
-			$catalog = Products::getAll([(int)$this->start, (int)$this->number], 'category');
-			$this->page += 1;
-			$numProduct = count(Products::getAll()) - $this->number;
-	
-			echo $this->render('catalog', ['catalog' => $catalog, 
-			'counter' => [
-				'numProduct' => $numProduct, 
-				'pagesLeft' => $this->page]
-				]);
-		}
+        $this->number = $this->page * $this->number;
+        $catalog = Products::getAll([(int) $this->start, (int) $this->number], 'category');
+        ++$this->page;
+        $numProduct = count(Products::getAll()) - $this->number;
 
-		public function actionItem(): void
-		{
-			$id = (int)$_GET['id'];
-			$product = Products::getOne($id);
-			echo $this->render('item', ['product' => $product]);
-		}
-	}
+        if ($numProduct < $this->loadItem) {
+            $this->loadItem = $numProduct;
+        }
+
+        echo $this->render('catalog',
+            ['catalog' => $catalog,
+                'counter' => [
+                    'numProduct' => $numProduct,
+                    'pagesLeft' => $this->page],
+                'item' => $this->loadItem]);
+    }
+
+    public function actionItem(): void
+    {
+        $id = (int) $_GET['id'];
+        $product = Products::getOne($id);
+        echo $this->render('item', ['product' => $product]);
+    }
+}
